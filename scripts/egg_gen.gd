c00623028061822cb19b89dba5e56279d4deb699
Nicholas Russell <thatnick@me.com>
2022-04-09 20:23:17 +0100

    Spawn eggs on a timer

--- scripts/egg_gen.gd
+++ scripts/egg_gen.gd
@@ -1,6 +1,10 @@
 extends Path2D
 
+const EGG = preload("res://scenes/egg.tscn")
+
 var speed = 250
+
+onready var game_scene = get_node("/root/Game")
 
 func _ready():
 	# stop at the end of the path instead of looping back to the start
@@ -12,3 +16,15 @@ func _process(delta):
 	#reverse when reaching the end of the path
 	if ($Bird.unit_offset >= 1 || $Bird.unit_offset <= 0):
 		speed = -speed
+
+func _on_EggSpawnTimer_timeout():
+	spawn_egg()
+
+func spawn_egg():
+	var egg = EGG.instance()
+	egg.position = $Bird.position
+	game_scene.add_child(egg)
+
+# Chang how frequently eggs are layed
+func change_spawn_interval(seconds):
+	$EggSpawnTimer.wait_time = seconds
