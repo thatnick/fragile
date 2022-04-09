e04140e909c968d1a55f6a546593ef4fcac40cee
Alistair <32274829+plyval@users.noreply.github.com>
2022-04-09 17:56:50 +0100

    Change Egg to static body on Floor collision

--- scripts/egg.gd
+++ scripts/egg.gd
@@ -2,3 +2,13 @@
 
 extends RigidBody2D
 
+
+
+func _on_Egg_body_entered(body):
+	if body.name == "Floor":
+		set_mode(MODE_STATIC)
+		print("mode was set to static")
+		#change mode to static
+	#if body entered == Nest AND !moving:
+		#hatch/ score points
+	pass 
