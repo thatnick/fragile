bf486f12670f7bd24bfea9c24453acf43e0e8639
Nicholas Russell <thatnick@me.com>
2022-04-09 20:31:12 +0100

    Improve comments

--- scripts/egg.gd
+++ scripts/egg.gd
@@ -9,6 +9,7 @@ func _process(delta):
 	# hatch the egg immediately if it has stopped moving inside the nest
 	if in_nest && !hatched && sleeping:
 		$HatchTimer.stop()
+		# stop the egg from moving, must be set after current frames physics step
 		set_deferred("mode", MODE_STATIC)
 		hatch()
 		
@@ -19,11 +20,10 @@ func _on_Egg_body_entered(body):
 # hatch the egg when the timer runs out (it starts on egg creation)
 func _on_HatchTimer_timeout():
 	hatch()
-	# TODO play an animation instead of the egg just disappearing
+	# TODO play an animation instead of the egg just deleting it
 	queue_free()
 
 func hit_floor():
-	# stop the egg from moving, must be set after current frames physics step
 	set_deferred("mode", MODE_STATIC)
 	# TODO change to the splat animation
 	
