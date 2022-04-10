5b51035f3766cb02a6fd790dfcba62304bb8ca05
Nicholas Russell <thatnick@me.com>
2022-04-10 17:23:59 +0100

    Remove unecessary pass statements

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,10 +10,7 @@ func get_input():
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	velocity = velocity.normalized() * SPEED
-	pass
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide(velocity * delta)
-	pass # Replace with function body.
-
