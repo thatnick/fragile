3d4f8321a757b51bd1a4480f572fb2e776b5ac06
Nicholas Russell <thatnick@me.com>
2022-04-10 17:31:10 +0100

    Fix lives not being set to lives total

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -9,7 +9,7 @@ var rng = RandomNumberGenerator.new()
 var score: int = 0
 var lives
 
-var lives_total = 3
+var lives_total = 3 setget set_lives_total
 var gravity = 1
 var vel_damp = 0.025
 var rot_vel_damp = 0.1
@@ -17,8 +17,12 @@ var egg_interval = 5
 
 func _ready():
 	rng.randomize()
-	lives = lives_total
+	#lives = lives_total
 	
 func _process(delta):
 	if (game_running && lives <= 0):
 		get_tree().change_scene("res://scenes/game_over.tscn")
+		
+func set_lives_total(new_value):
+	lives_total = new_value
+	lives = lives_total
