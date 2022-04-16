ed7b9d2946253eceaf6d725b4358926281a30ca5
Nicholas Russell <thatnick@me.com>
2022-04-16 12:45:20 +0100

    Make nest kinematic and moveable. Remove bounce on space bar.

--- nest/nest.gd
+++ nest/nest.gd
@@ -1,2 +1,19 @@
 class_name Nest
-extends StaticBody2D
+extends KinematicBody2D
+
+onready var manager = get_node("/root/Manager")
+
+const SPEED = 500
+
+var velocity = Vector2()
+
+func get_input():
+	velocity.x = 0;
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += SPEED
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= SPEED
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity, Vector2(0, -1))
