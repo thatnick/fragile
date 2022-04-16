87da488037e3daf30822d6fd22cef45deb088623
Nicholas Russell <thatnick@me.com>
2022-04-16 12:40:48 +0100

    Remove references to lives from HUD. Add timer placeholder.

--- game/hud.gd
+++ game/hud.gd
@@ -2,13 +2,7 @@ extends CanvasLayer
 class_name HUD
 
 func _ready():
-	$TotalLivesLabel.text = str(get_tree().current_scene.total_lives)
 	$TargetScoreLabel.text = str(get_tree().current_scene.target_score)
 	
 func _process(_delta):
 	$CurrentScoreLabel.text = str(get_tree().current_scene.current_score)
-	$CurrentLivesLabel.text = str(get_tree().current_scene.current_lives)
-	
-	# DEBUG
-	$TotalLivesLabel.text = str(get_tree().current_scene.total_lives)
-
