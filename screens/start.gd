f6a11d467febc8e99dc4ad2048b88d09e1449b11
Nicholas Russell <thatnick@me.com>
2022-04-16 21:20:30 +0100

    Update start screen UI

--- screens/start.gd
+++ screens/start.gd
@@ -1,10 +1,6 @@
-class_name Start
-
-extends Control
+extends CanvasLayer
 
 onready var manager: Node = get_node("/root/Manager")
 
-func _process(delta) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		manager.new_game()
-
+func _on_NewGameButton_pressed():
+	manager.new_game()
