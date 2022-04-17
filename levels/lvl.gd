2eee0f5caf335527634c9cb6f412335168848156
Nicholas Russell <thatnick@me.com>
2022-04-17 17:26:22 +0100

    Add score grading to level complete

--- levels/lvl.gd
+++ levels/lvl.gd
@@ -17,14 +17,14 @@ var bomb_drop_interval = 5
 # For every egg dropped, there will be this many bombs dropped
 var bombs_per_egg = 0.25
 var current_score = 0
-var target_score
+var final_score
 
 var level_timer = Timer.new()
 var level_complete_timer = Timer.new()
 var time_left
 
 func _ready():
-	calc_target_score()
+	calc_target_scores()
 	setup_timers()
 	start_level()
 	manager.level_running = true
@@ -36,15 +36,22 @@ func _exit_tree():
 	manager.level_running = false
 	
 func _on_level_timer_timeout():
+	final_score.actual = current_score
 	level_complete()
 
 func _on_level_complete_timer_timeout():
 	get_tree().paused = false
-	manager.level_complete()
+	manager.level_complete(final_score)
 
-func calc_target_score():
+func calc_target_scores():
 	var max_score = (time_limit_secs - EGG_FALL_TIME_SECS) / egg_lay_interval
-	target_score = max_score * SCORE_MULTIPLIER * (GOLD_SCORE_PERCENT / 100.0)
+	final_score = {
+		"gold": max_score * SCORE_MULTIPLIER * (GOLD_SCORE_PERCENT / 100.0),
+		"silver": max_score * SCORE_MULTIPLIER * (SILVER_SCORE_PERCENT / 100.0),
+		"bronze": max_score * SCORE_MULTIPLIER * (BRONZE_SCORE_PERCENT / 100.0),
+		"max": max_score,
+		"actual": 0
+	}
 
 func setup_timers():
 	level_timer.one_shot = true
