b6290dcb3fc32c884142e2e794941eef65664dae
Alistair <32274829+plyval@users.noreply.github.com>
2022-04-17 17:38:16 +0100

    Alternate music between levels

--- music_player/music_player.gd
+++ music_player/music_player.gd
@@ -6,12 +6,19 @@ var loop_b: Resource = preload("res://music_player/music/loop_b.mp3")
 var loop_c: Resource = preload("res://music_player/music/loop_c.mp3")
 var fast_loop_a: Resource = preload("res://music_player/music/fast_loop_a.mp3")
 
+var played_loop_b: bool = false
+
 func menu_music():
 	set_stream(start_menu_music)
 	play()
 
 func lvl_music():
-	set_stream(loop_b)
+	if played_loop_b:
+		set_stream(loop_c)
+		played_loop_b = false
+	elif !played_loop_b:
+		set_stream(loop_b)
+		played_loop_b = true
 	play()
 
 func pause_music():
