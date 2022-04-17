bb1ef8d93bf65010289b290c3323f29853b178b4
Alistair <32274829+plyval@users.noreply.github.com>
2022-04-17 14:53:15 +0100

    Make bombs only affect chicks on ground

--- explosion/explosion.gd
+++ explosion/explosion.gd
@@ -12,11 +12,12 @@ func _ready():
 
 func _on_Explosion_body_entered(body):
 	if body is Chick:
-		if body.get_position().x >= position.x:
-			explosion_vector.x = 1
-		else:
-			explosion_vector.x = -1
-		body.explosion_impulse(explosion_vector, EXPLOSION_FORCE)
+		if body.on_floor:
+			if body.get_position().x >= position.x:
+				explosion_vector.x = 1
+			else:
+				explosion_vector.x = -1
+			body.explosion_impulse(explosion_vector, EXPLOSION_FORCE)
 
 
 func _on_AnimatedSprite_animation_finished():
